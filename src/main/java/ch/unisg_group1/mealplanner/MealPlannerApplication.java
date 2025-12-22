87b0877a85113e490906de6361a0d1d098d54004
Thierry Suhner <70145579+thierrysuhner@users.noreply.github.com>
2025-12-22 19:38:28 +0100

    Added Spring Initializr Files

---
+++ src/main/java/ch/unisg_group1/mealplanner/MealPlannerApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg_group1.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/unisg_group1/mealplanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.unisg_group1.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
