6f474ce79a8691fa9f6842a3a1eed9939bf7cb96
Thierry Suhner <thierry.suhner@gmx.ch>
2026-06-01 00:47:51 +0200

    Added the fucking shell of the App

--- src/main/java/ch/unisg_group1/mealplanner/AppShell.java
+++
@@ -1,7 +0,0 @@
-package ch.unisg_group1.mealplanner;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.component.page.AppShellConfigurator;
-
-@PageTitle("Meal Planner")
-public class AppShell implements AppShellConfigurator {
-}
--- src/main/java/ch/unisg_group1/mealplanner/MealPlannerApplication.java
+++ src/main/java/ch/unisg_group1/mealplanner/MealPlannerApplication.java
@@ -4,7 +4,10 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.page.AppShellConfigurator;
 
+@PageTitle("Meal Planner")
 @SpringBootApplication
 public class MealPlannerApplication implements AppShellConfigurator {
 
