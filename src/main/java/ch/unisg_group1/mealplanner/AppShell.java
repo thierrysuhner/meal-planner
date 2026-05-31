a5333ce99ca0a1ea1766fe13ff9ea90f79f9fdc4
Thierry Suhner <thierry.suhner@gmx.ch>
2026-06-01 00:42:15 +0200

    Added the fucking shell of the App

---
+++ src/main/java/ch/unisg_group1/mealplanner/AppShell.java
@@ -0,0 +1,7 @@
+package ch.unisg_group1.mealplanner;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+
+@PageTitle("Meal Planner")
+public class AppShell implements AppShellConfigurator {
+}
